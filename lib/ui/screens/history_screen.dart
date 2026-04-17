0a1dd859aa78bc004068eb3594fe97d1d3ba2dcf
Manuel Moscoso <mmoscoso@utalca.cl>
2026-04-16 23:18:43 -0400

    Primera version de la lista con Card en lugar de ListTile

--- lib/ui/screens/history_screen.dart
+++ lib/ui/screens/history_screen.dart
@@ -11,27 +11,57 @@ class HistoryScreen extends StatelessWidget {
     difficulty: 'Medio',
   );
 
+  // En lib/ui/screens/history_screen.dart, dentro del método build()
+    final List<GameResult> listMaqueta = [
+      GameResult(date: '10 Abr', timeSpent: '01:20', isVictory: true, difficulty: 'Fácil'),
+      GameResult(date: '09 Abr', timeSpent: '04:45', isVictory: false, difficulty: 'Difícil'),
+      GameResult(date: '08 Abr', timeSpent: '02:10', isVictory: true, difficulty: 'Medio')
+    ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Historial (Maqueta)')),
-      body: Center(
-        child: 
-         Card(
-          elevation: 4,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Fecha: ${game.date}'),
-                Text('Tiempo: ${game.timeSpent}')
-              ],
-            ),
-          ),
-         ) 
-          
-      ),
+      body: 
+        ListView.builder(itemBuilder: (context, index) {
+          final game = listMaqueta[index];
+          return GameResultUI(game: game); // GameResultUI es el widget personalizado que creamos en los pasos anteriores (refactor) 
+        },
+        itemCount: listMaqueta.length,
+        ),
     );
+  }
+}
+
+class GameResultUI extends StatelessWidget {
+  const GameResultUI({
+    super.key,
+    required this.game,
+  });
+
+  final GameResult game;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      
+    child: 
+     Card(
+      
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Fecha: ${game.date}'),
+            Text('Tiempo: ${game.timeSpent}')
+          ],
+        ),
+      ),
+     ) 
+      
+          );
   }
 }
